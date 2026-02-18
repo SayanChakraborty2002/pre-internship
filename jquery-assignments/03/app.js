814a77bd27c77312e2b3e6a89056a699f900eb96
Sayan Chakraborty <sayanchakraborty748@gmail.com>
2026-02-18 12:07:36 +0530

    Done JQuery Assignments

--- jquery-assignments/03/app.js
+++ jquery-assignments/03/app.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
-  let isEmpty = false;
   $("form").on("submit", function () {
-    $(this)
-      .find(":input")
-      .not(":button", ":submit")
-      .each(function () {
-        if (!$(this).val().trim()) {
-          isEmpty = true;
-          return false;
-        }
-      });
+    $("input").each(function (i, ele) {
+      if ($(ele).val().trim() == "") {
+        isEmpty = true;
+        return false;
+      }
+    });
     if (isEmpty) {
       alert("Please fill all the fields");
     }
